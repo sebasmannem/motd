28adf010d62d7f31e46e3747c3ae98f565c3a157
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 12:59:44 +0200

    We should move the memes code to its own module and keep it in the pkg folder That way the code can be reused by other modules in this project and even by other projects. This keeps the cmd code cleaner. And we could (as an example) reuse this code in another cmd such as motd-web.

--- cmd/motd-cli/main.go
+++ cmd/motd-cli/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/sebasmannem/motd/pkg/memes"
+)
 
 func main() {
-	if memeDir, err := NewMemeDir("./testdata/"); err != nil {
+	if memeDir, err := memes.NewMemeDir("./testdata/"); err != nil {
 		log.Fatalf("Houston, we have a problem: %e", err)
 	} else {
 		memeDir.Print()
