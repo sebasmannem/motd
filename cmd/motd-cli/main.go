3783ad42a2ecfac9957c118fac36944bfa2a7abc
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 11:54:19 +0200

    use seperate files for separate functionalities is even more cleander

--- cmd/motd-cli/main.go
+++ cmd/motd-cli/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-)
-
-func printMemeFiles(path string) {
-	var entries []os.DirEntry
-	var err error
-	if entries, err = os.ReadDir(path); err != nil {
-		log.Fatal(err)
-	} else {
-		for _, e := range entries {
-			fmt.Println(filepath.Join(path, e.Name()))
-		}
-	}
-}
-
 func main() {
 	printMemeFiles("./testdata/")
 }
