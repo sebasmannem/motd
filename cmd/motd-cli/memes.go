07601e27f1e00d3a45212a2d0d51a7a438a4ced7
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 15:28:14 +0200

    using MemeFile as a struct

--- cmd/motd-cli/memes.go
+++ cmd/motd-cli/memes.go
@@ -8,8 +8,21 @@ import (
 	"path/filepath"
 )
 
-type MemeFile string
+type MemeFile struct {
+	name string
+	path string
+}
+
+func (mf MemeFile) String() string {
+	return filepath.Join(mf.path, mf.name)
+}
+
 type MemeFiles []MemeFile
+
+func (mfs MemeFiles) getRandom() MemeFile {
+	return mfs[rand.Intn(len(mfs))]
+}
+
 type MemeDir struct {
 	path  string
 	files MemeFiles
@@ -31,7 +44,10 @@ func (md *MemeDir) getFiles() MemeFiles {
 		log.Fatal(err)
 	} else {
 		for _, entry := range entries {
-			md.files = append(md.files, MemeFile(filepath.Join(md.path, entry.Name())))
+			md.files = append(md.files, MemeFile{
+				path: md.path,
+				name: entry.Name(),
+			})
 		}
 		return md.files
 	}
@@ -46,14 +62,12 @@ func (md *MemeDir) Print() {
 		for _, entry := range entries {
 			fmt.Printf("- %s\n", entry)
 		}
+		// md.GetRandomFile() returns a MemeFile, but that has a String() method,
+		// which is automatically used by fmt.Printf("%s")
 		fmt.Printf("This is the one:\n%s", md.GetRandomFile())
 	}
 }
 
 func (md MemeDir) GetRandomFile() MemeFile {
 	return md.files.getRandom()
-}
-
-func (mfs MemeFiles) getRandom() MemeFile {
-	return mfs[rand.Intn(len(mfs))]
 }
