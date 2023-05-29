4ee2006e04bb273e7fc441060a60fc4e0cf9fb7c
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 11:54:19 +0200

    use seperate files for separate functionalities is even more cleander

---
+++ cmd/motd-cli/memes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func printMemeFiles(path string) {
+	var entries []os.DirEntry
+	var err error
+	if entries, err = os.ReadDir(path); err != nil {
+		log.Fatal(err)
+	} else {
+		for _, e := range entries {
+			fmt.Println(filepath.Join(path, e.Name()))
+		}
+	}
+}
