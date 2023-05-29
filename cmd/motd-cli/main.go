a6b8f5541b4d0e42a8a5f76af1141009d7d7a49d
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 12:08:23 +0200

    Lets add a getRandomMeme function. Separating out reusable code prevents duplication. Also note fmt.Prinf in main.go

--- cmd/motd-cli/main.go
+++ cmd/motd-cli/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	printMemeFiles("./testdata/")
+	memeLocation := "./testdata/"
+	println("Out of all these meme files:")
+	printMemeFiles(memeLocation)
+	fmt.Printf("This is the one:\n%s", getRandomMemeFile(memeLocation))
 }
