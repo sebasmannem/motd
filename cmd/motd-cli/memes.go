4725c1b99ad35fd92cc2ada0f1ace3d78ecc9ccb
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 12:08:23 +0200

    Lets add a getRandomMeme function. Separating out reusable code prevents duplication. Also note fmt.Prinf in main.go

--- cmd/motd-cli/memes.go
+++ cmd/motd-cli/memes.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
 )
 
-func printMemeFiles(path string) {
+func getMemeFiles(path string) []string {
+	var memeFiles []string
 	var entries []os.DirEntry
 	var err error
 	if entries, err = os.ReadDir(path); err != nil {
 		log.Fatal(err)
 	} else {
 		for _, e := range entries {
-			fmt.Println(filepath.Join(path, e.Name()))
+			memeFiles = append(memeFiles, filepath.Join(path, e.Name()))
+		}
+		return memeFiles
+	}
+	return nil
+}
+
+func printMemeFiles(path string) {
+	if entries := getMemeFiles(path); entries != nil {
+		for _, entry := range entries {
+			fmt.Println(entry)
 		}
 	}
+}
+
+func getRandomMemeFile(path string) string {
+	if entries := getMemeFiles(path); entries != nil {
+		return entries[rand.Intn(len(entries))]
+	}
+	return ""
 }
