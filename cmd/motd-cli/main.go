9fef5ac5284d8ddc29e3141d92178ac25bb9199d
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-29 16:16:38 +0200

    Adding an option for kitty

--- cmd/motd-cli/main.go
+++ cmd/motd-cli/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	imgview "github.com/sebasmannem/motd/pkg/img_view"
+	kitty "github.com/sebasmannem/motd/pkg/kitty-display"
 	"github.com/sebasmannem/motd/pkg/memes"
 )
 
@@ -11,6 +13,11 @@ func main() {
 	if memeDir, err := memes.NewMemeDir("./testdata/"); err != nil {
 		log.Fatalf("Houston, we have a problem: %e", err)
 	} else {
-		imgview.DrawOnConsole(memeDir.GetRandomFile().String())
+		path := memeDir.GetRandomFile().String()
+		if _, is_defined := os.LookupEnv("KITTY_WINDOW_ID"); is_defined {
+			kitty.FromFile(path)
+		} else {
+			imgview.DrawOnConsole(path)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/sebasmannem/motd
 go 1.20
 
 require (
+	github.com/dolmen-go/kittyimg v0.0.0-20220904140504-22f6493b700f // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nsf/termbox-go v1.1.1 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
 )
